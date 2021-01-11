ce384ec2a093e0273d4a0b57b7d581b2d640728f
William Sundgård Anker <will1331@edu.sde.dk>
2021-01-11 12:04:29 +0100

    0.0.0.1 creating a projekt called KrigCardGame

---
+++ KrigCardGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KrigCardGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
