ff50e6fd8bf25c57970a997852f13f4339016f5a
William Sundgård Anker <will1331@edu.sde.dk>
2021-01-12 09:33:56 +0100

    0.0.0.5 created the method deal in Player and the method Game and IsGameOver in Game.

--- KrigCardGame/Game.cs
+++ KrigCardGame/Game.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace KrigCardGame
@@ -10,5 +11,41 @@ namespace KrigCardGame
         private Player Player2 { get; set; }
         private int TurnCount { get; set; } // number of turns played
 
+        public Game(string player1name, string player2name)
+        {
+            Player player1 = new Player(player1name);
+            Player player2 = new Player(player2name);
+
+            var Cards = DeckCreator.CreateCards(); // returns a shuffled deck
+
+            var deck = player1.Deal(Cards); // returns player2's deck and player1 keeps his
+
+            player2.Deck = deck;
+        }
+
+
+
+
+        public bool IsGameOver()
+        {
+            if (!Player1.Deck.Any())
+            {
+                Console.WriteLine(Player1.PlayerName + "is out of cards" 
+                    + Player2.PlayerName + "WINS!");
+                return true;
+            }
+            else if (!Player2.Deck.Any())
+            {
+                Console.WriteLine(Player2.PlayerName + "is out of cards"
+                    + Player1.PlayerName + "WINS!");
+                return true;
+            }
+            else if (TurnCount > 2000)
+            {
+                Console.WriteLine("Infinite game! lets call this whole thing off");
+                return true;
+            }
+            return false;
+        }
     }
 }
--- KrigCardGame/Player.cs
+++ KrigCardGame/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace KrigCardGame
@@ -8,5 +9,42 @@ namespace KrigCardGame
     {
         public string PlayerName { get; set; }
         public Queue<Card> Deck { get; set; }
+
+        public Player()
+        {
+
+        }
+
+        public Player(String name)
+        {
+            PlayerName = name;
+        }
+
+        public Queue<Card> Deal(Queue<Card> cards)
+        {
+            Queue<Card> player1Cards = new Queue<Card>();
+            Queue<Card> player2Cards = new Queue<Card>();
+
+            int counter = 2;
+            // runs the code until there is no more cards in the Queue
+            while (cards.Any())
+            {
+                
+                if (counter % 2 == 0)
+                {
+                    // adds an element to player2Cards and removes
+                    // that card from the cards queue
+                    player2Cards.Enqueue(cards.Dequeue());
+                }
+                else
+                {
+                    player1Cards.Enqueue(cards.Dequeue());
+                }
+                counter++;
+            }
+
+            Deck = player1Cards;
+            return player2Cards;
+        }
     }
 }
